743620e90919171a8b099bf61155bb3f3d4bd095
GITnvincAdonis <rav30nad0nis@gmail.com>
2024-10-17 18:55:01 -0400

    database connection in backend

--- server/db.js
+++ server/db.js
@@ -1,12 +1,7 @@
 const Pool = require("pg").Pool;
 require('dotenv').config();
-const pool = new Pool({
-    user: "postgres",
-    password: "0129",
-    host: "localhost",
-    port: 5432,
-    database: "item_database"
 
-});
+const urlDB = `postgresql://postgres:MQnoHiecvPTQzOusLbqcRPwGgYdqrNNI@autorack.proxy.rlwy.net:54083/railway`
+const pool = new Pool(urlDB);
 
 module.exports = pool;
