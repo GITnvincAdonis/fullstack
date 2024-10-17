e960c260e7887cc04e49fc6ddb25d9653546337c
GITnvincAdonis <rav30nad0nis@gmail.com>
2024-10-17 19:21:26 -0400

    ad

--- server/db.js
+++ server/db.js
@@ -1,7 +1,13 @@
 const Pool = require("pg").Pool;
 require('dotenv').config();
 
-const urlDB = `postgresql://postgres:MQnoHiecvPTQzOusLbqcRPwGgYdqrNNI@autorack.proxy.rlwy.net:54083/railway`
-const pool = new Pool(urlDB);
+
+const pool = new Pool({
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_DATABASE
+});
 
 module.exports = pool;
