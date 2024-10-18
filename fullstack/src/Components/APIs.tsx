bf4431667b7cbd2b04025ff4964e0c82549f3fbe
GITnvincAdonis <rav30nad0nis@gmail.com>
2024-10-18 17:15:03 -0400

    finding error

--- fullstack/src/Components/APIs.tsx
+++ fullstack/src/Components/APIs.tsx
@@ -1,18 +1,20 @@
-
-export async function GetItems() {
+interface itemInfo {
+  id: number;
+  name: string;
+  price: number;
+  star_count: number;
+}
+export const GetItems = async (): Promise<itemInfo[]> => {
   try {
-    
     const items = await fetch(
       `https://fullstack-production-95cf.up.railway.app/products`
     );
     const data = items.json();
 
-    
     console.log(data);
     return data;
-    
   } catch (error) {
     console.log(error);
-    return [1, 1, 1, 1];
+    return [];
   }
-}
+};
--- fullstack/src/Components/ProductPage/ProductPage.tsx
+++ fullstack/src/Components/ProductPage/ProductPage.tsx
@@ -6,34 +6,9 @@ import Footer from "../Footer/Footer";
 
 import { useQuery } from "@tanstack/react-query";
 import { GetItems } from "../APIs";
-import { Key, useState } from "react";
-
-interface itemInfo {
-  id: number;
-  name: string;
-  price: number;
-  star_count: number;
-}
-
-function setCards(info: any) {
-  if (!info || !Array.isArray(info)) {
-    return []; // Return an empty array if `info` is undefined or not an array
-  }
-  const dataContainer = info.map(
-    (item: { id: number; name: string; price: number; starcount: number }) => ({
-      id: item.id,
-      name: item.name,
-      price: item.price,
-      star_count: item.starcount,
-    })
-  );
-  return dataContainer;
-}
 
 //const products = [1, 1, 1, 1, 1, 1];
 export default function ProductPage() {
-  const [objects, SetObjects] = useState<itemInfo[]>([]);
-  
   const {
     data: info,
     isLoading,
@@ -48,22 +23,21 @@ export default function ProductPage() {
   if (isError) return <div>{`error: ${error}`}</div>;
   if (isLoading) return <div>is loading....</div>;
 
-  SetObjects(info ? setCards(info) : []);
   return (
     <>
       <Navbar></Navbar>
 
       <div className="product-page-container mb-5 ">
         <h1 className="product-page-title ">SHOP _SPECIFIC_ PRODUCTS</h1>
-        {objects?.map((item: any, index: Key) => {
-          const name = item.name;
-          const id = item.id;
-          const price = item.price;
-          const stars = item.star_count;
-          console.log(id);
+        {info?.map((info) => {
+          console.log(info.id);
           return (
-            <span key={index}>
-              <Card name={name} price={price} starCount={stars} />
+            <span>
+              <Card
+                name={info.name}
+                price={info.price}
+                starCount={info.star_count}
+              />
             </span>
           );
         })}
