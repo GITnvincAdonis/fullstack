9cb9e5639441fae991f454c873d5728c6bb14c9a
GITnvincAdonis <rav30nad0nis@gmail.com>
2024-10-19 20:20:45 -0400

    moving client

--- fullstack/src/App.tsx
+++ fullstack/src/App.tsx
@@ -13,22 +13,27 @@ const Client = new QueryClient();
 function App() {
   return (
     <>
-      <QueryClientProvider client={Client}>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <Navbar />
-                <Homepage />
-              </>
-            }
-          />
-          <Route path="/home" element={<ProductPage />} />
-          <Route path="/item" element={<ItemPage />} />
-          <Route path="/Checkout" element={<CheckoutPage />} />
-        </Routes>
-      </QueryClientProvider>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Navbar />
+              <Homepage />
+            </>
+          }
+        />
+        <Route
+          path="/home"
+          element={
+            <QueryClientProvider client={Client}>
+              <ProductPage />
+            </QueryClientProvider>
+          }
+        />
+        <Route path="/item" element={<ItemPage />} />
+        <Route path="/Checkout" element={<CheckoutPage />} />
+      </Routes>
     </>
   );
 }
