bab226224febc49a5e4148ff9e1b4e247fa31b06
GITnvincAdonis <rav30nad0nis@gmail.com>
2024-11-01 13:09:42 -0400

    error inn logic, if loading state is dependant on images and images are never rendered the system is dealocked

--- fullstack/src/Components/ProductPage/ProductPage.tsx
+++ fullstack/src/Components/ProductPage/ProductPage.tsx
@@ -9,7 +9,7 @@ import { useQuery } from "@tanstack/react-query";
 import { GetItems } from "../APIs";
 import { CVid } from "../Cloudinary/CloudinaryAssets";
 import { useState } from "react";
-
+import { motion } from "framer-motion";
 
 //const products = [1, 1, 1, 1, 1, 1];
 export default function ProductPage() {
@@ -30,7 +30,7 @@ export default function ProductPage() {
   }
   if (isError) return <div>{`error: ${error}`}</div>;
 
-  if (isLoading || (info && loadedCards < info.length))
+  if (isLoading)
     return (
       <>
         <Navbar></Navbar>
@@ -57,41 +57,70 @@ export default function ProductPage() {
 
   return (
     <>
-      <Navbar></Navbar>
+      {info && loadedCards < info.length && (
+        <>
+          <Navbar></Navbar>
 
-      <div className="product-page-container mb-5 ">
-        <div className=" top-gradient-container">
-          <div className="top-gradient"></div>
+          <div className="product-page-container mb-5 ">
+            <div className=" top-gradient-container">
+              <div className="top-gradient"></div>
+            </div>
+            <div className=" d-flex justify-content-center align-items-center vid-placeholder-wrapper ">
+              <div className=" placeholder vid-placeholder placeholder-wave"></div>
+            </div>
+            <h1 className="product-page-title ">SHOP _SPECIFIC_ PRODUCTS</h1>
+
+            <DefaultCard />
+            <DefaultCard />
+            <DefaultCard />
+            <DefaultCard />
+            <DefaultCard />
+            <DefaultCard />
+          </div>
+          <Footer></Footer>
+        </>
+      )}
+      <motion.div
+        animate={
+          info && loadedCards < info.length ? { opacity: 0 } : { opacity: 1 }
+        }
+      >
+        <Navbar></Navbar>
+
+        <div className="product-page-container mb-5 ">
+          <div className=" top-gradient-container">
+            <div className="top-gradient"></div>
+          </div>
+          <div className=" d-flex justify-content-center align-items-center  video-mega-container ">
+            <CVid
+              classNames={"video-container m-0"}
+              CloudinaryVideoID="y8iuviswnbl0epscimhb"
+            ></CVid>
+          </div>
+
+          <h1 className="product-page-title ">SHOP _SPECIFIC_ PRODUCTS</h1>
+
+          {info?.map((info) => {
+            console.log("star count " + info.starcount);
+            console.log("review count" + info.review_count);
+
+            return (
+              <span>
+                <Card
+                  image_fn={IncremementLoadCards}
+                  id={info.id}
+                  name={info.name}
+                  price={info.price}
+                  starCount={info.starcount}
+                  reviewNumber={info.review_count}
+                  image_pub_id={info.image_pub_id}
+                />
+              </span>
+            );
+          })}
         </div>
-        <div className=" d-flex justify-content-center align-items-center  video-mega-container ">
-          <CVid
-            classNames={"video-container m-0"}
-            CloudinaryVideoID="y8iuviswnbl0epscimhb"
-          ></CVid>
-        </div>
-
-        <h1 className="product-page-title ">SHOP _SPECIFIC_ PRODUCTS</h1>
-
-        {info?.map((info) => {
-          console.log("star count " + info.starcount);
-          console.log("review count" + info.review_count);
-
-          return (
-            <span>
-              <Card
-                image_fn={IncremementLoadCards}
-                id={info.id}
-                name={info.name}
-                price={info.price}
-                starCount={info.starcount}
-                reviewNumber={info.review_count}
-                image_pub_id={info.image_pub_id}
-              />
-            </span>
-          );
-        })}
-      </div>
-      <Footer></Footer>
+        <Footer></Footer>
+      </motion.div>
     </>
   );
 }
