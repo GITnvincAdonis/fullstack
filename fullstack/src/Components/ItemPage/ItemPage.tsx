c72dd43b3385157234e6ad6dbd568a90e9cf0216
GITnvincAdonis <rav30nad0nis@gmail.com>
2024-11-01 18:04:34 -0400

    debugging image feedback for paged item

--- fullstack/src/Components/ItemPage/ItemPage.tsx
+++ fullstack/src/Components/ItemPage/ItemPage.tsx
@@ -11,7 +11,6 @@ import { CImage } from "../Cloudinary/CloudinaryAssets";
 import { motion } from "framer-motion";
 import PageLoader from "../PageLoader";
 
-
 interface itemInfo {
   id: number;
   name: string;
@@ -62,54 +61,48 @@ export default function ItemPage() {
   }, [loadedIms]);
   return (
     <>
-      {retrievedPagedItem && (
-        <>
-          <Navbar></Navbar>
+      <Navbar></Navbar>
 
-          <motion.div
-            initial={{ opacity: 1 }}
-            animate={loadedIms < 1 ? { opacity: 1 } : { opacity: 0 }}
-            transition={{ delay: 30 }}
-          >
-            <PageLoader></PageLoader>
-          </motion.div>
+      <motion.div
+        initial={{ opacity: 1 }}
+        animate={loadedIms < 1 ? { opacity: 1 } : { opacity: 0 }}
+        transition={{ delay: 30 }}
+      >
+        <PageLoader></PageLoader>
+      </motion.div>
 
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={!(loadedIms === 1) ? { opacity: 0 } : { opacity: 1 }}
-            transition={{ duration: 0.3 }}
-          >
-            <div className=" product-item-container">
-              <CImage
-                loadFunc={handleLoading}
-                CloudinaryImageID={`${retrievedPagedItem.image_pub_id}`}
-                image_size={1900}
-                classNames="item-image"
-              ></CImage>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={!(loadedIms === 1) ? { opacity: 0 } : { opacity: 1 }}
+        transition={{ duration: 0.3 }}
+      >
+        <div className=" product-item-container">
+          <CImage
+            loadFunc={handleLoading}
+            CloudinaryImageID={`${retrievedPagedItem?.image_pub_id}`}
+            image_size={1900}
+            classNames="item-image"
+          ></CImage>
 
-              <div className="item-body d-flex flex-column align-items-start ">
-                <h1 className="item-name">{retrievedPagedItem.name}</h1>
-                <Review reviewNumber={retrievedPagedItem.review_count}></Review>
-                <h3 className="product-price m-0">
-                  ${retrievedPagedItem.price}
-                </h3>
-                <h3>ingredient list</h3>
-                <p className="ingredient-list">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Obcaecati saepe distinctio minima officia quisquam voluptate
-                  blanditiis sunt, laborum est eos sapiente magnam recusandae a
-                  odit molestias sit tempore numquam quidem. In voluptates,
-                  veritatis sed soluta dolor autem animi eaque nobis. Officiis
-                  quisquam inventore voluptatem, doloribus, dignissimos
-                  explicabo incidunt magni provident officia quod, at ea eaque
-                  tempore ipsum vero. Facilis, quasi?
-                </p>
-                <ItemInstruction></ItemInstruction>
-              </div>
-            </div>
-          </motion.div>
-        </>
-      )}
+          <div className="item-body d-flex flex-column align-items-start ">
+            <h1 className="item-name">{retrievedPagedItem?.name}</h1>
+            <Review reviewNumber={retrievedPagedItem?.review_count}></Review>
+            <h3 className="product-price m-0">${retrievedPagedItem?.price}</h3>
+            <h3>ingredient list</h3>
+            <p className="ingredient-list">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
+              saepe distinctio minima officia quisquam voluptate blanditiis
+              sunt, laborum est eos sapiente magnam recusandae a odit molestias
+              sit tempore numquam quidem. In voluptates, veritatis sed soluta
+              dolor autem animi eaque nobis. Officiis quisquam inventore
+              voluptatem, doloribus, dignissimos explicabo incidunt magni
+              provident officia quod, at ea eaque tempore ipsum vero. Facilis,
+              quasi?
+            </p>
+            <ItemInstruction></ItemInstruction>
+          </div>
+        </div>
+      </motion.div>
     </>
   );
 }
