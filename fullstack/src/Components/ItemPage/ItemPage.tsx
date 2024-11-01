4a9fe77e771d76672294857d8cb98e1a20f1b3fb
GITnvincAdonis <rav30nad0nis@gmail.com>
2024-11-01 16:12:17 -0400

    debugging fetching

--- fullstack/src/Components/ItemPage/ItemPage.tsx
+++ fullstack/src/Components/ItemPage/ItemPage.tsx
@@ -36,7 +36,7 @@ export default function ItemPage() {
 
     if (data) {
       console.log("paged item");
-      console.log([data].flat(2));
+      console.log([data].flat(1));
       setItem(data);
     }
     console.log("state item");
