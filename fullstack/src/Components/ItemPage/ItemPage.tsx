a9a7140115f4cdb1c2391aa9ee320240a8fae321
GITnvincAdonis <rav30nad0nis@gmail.com>
2024-11-01 17:38:15 -0400

    debugging image feedback for paged item

--- fullstack/src/Components/ItemPage/ItemPage.tsx
+++ fullstack/src/Components/ItemPage/ItemPage.tsx
@@ -68,46 +68,49 @@ export default function ItemPage() {
       {retrievedPagedItem && (
         <>
           <Navbar></Navbar>
-          <div className=" product-item-container">
-            <div className="loader-wrapper">
-              <motion.div
-                animate={!isLoaded ? { opacity: 1 } : { opacity: 0 }}
-                transition={{ delay: 2 }}
-              >
-                <PageLoader></PageLoader>
-              </motion.div>
-            </div>
+          <div className="loader-wrapper">
             <motion.div
-              initial={{ opacity: 0 }}
-              animate={!isLoaded ? { opacity: 0 } : { opacity: 1 }}
-              transition={{ duration: 1 }}
+              animate={!isLoaded ? { opacity: 1 } : { opacity: 0 }}
+              transition={{ delay: 2 }}
             >
+              <PageLoader></PageLoader>
+            </motion.div>
+          </div>
+
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={!isLoaded ? { opacity: 0 } : { opacity: 1 }}
+            transition={{ duration: 1 }}
+          >
+            <div className=" product-item-container">
               <CImage
                 loadFunc={handleLoading}
                 CloudinaryImageID={`${retrievedPagedItem.image_pub_id}`}
                 image_size={1900}
                 classNames="item-image"
               ></CImage>
-            </motion.div>
 
-            <div className="item-body d-flex flex-column align-items-start ">
-              <h1 className="item-name">{retrievedPagedItem.name}</h1>
-              <Review reviewNumber={retrievedPagedItem.review_count}></Review>
-              <h3 className="product-price m-0">${retrievedPagedItem.price}</h3>
-              <h3>ingredient list</h3>
-              <p className="ingredient-list">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Obcaecati saepe distinctio minima officia quisquam voluptate
-                blanditiis sunt, laborum est eos sapiente magnam recusandae a
-                odit molestias sit tempore numquam quidem. In voluptates,
-                veritatis sed soluta dolor autem animi eaque nobis. Officiis
-                quisquam inventore voluptatem, doloribus, dignissimos explicabo
-                incidunt magni provident officia quod, at ea eaque tempore ipsum
-                vero. Facilis, quasi?
-              </p>
-              <ItemInstruction></ItemInstruction>
+              <div className="item-body d-flex flex-column align-items-start ">
+                <h1 className="item-name">{retrievedPagedItem.name}</h1>
+                <Review reviewNumber={retrievedPagedItem.review_count}></Review>
+                <h3 className="product-price m-0">
+                  ${retrievedPagedItem.price}
+                </h3>
+                <h3>ingredient list</h3>
+                <p className="ingredient-list">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Obcaecati saepe distinctio minima officia quisquam voluptate
+                  blanditiis sunt, laborum est eos sapiente magnam recusandae a
+                  odit molestias sit tempore numquam quidem. In voluptates,
+                  veritatis sed soluta dolor autem animi eaque nobis. Officiis
+                  quisquam inventore voluptatem, doloribus, dignissimos
+                  explicabo incidunt magni provident officia quod, at ea eaque
+                  tempore ipsum vero. Facilis, quasi?
+                </p>
+                <ItemInstruction></ItemInstruction>
+              </div>
             </div>
-          </div>
+          </motion.div>
         </>
       )}
     </>
