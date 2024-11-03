750820edbd6b45f3425877d2bac6d1f051ea28aa
GITnvincAdonis <rav30nad0nis@gmail.com>
2024-11-03 18:50:08 -0400

    issue with backend end point concat

--- server/index.js
+++ server/index.js
@@ -36,9 +36,9 @@ app.get("/products", async(req, res)=>{
 
 app.get("/products/search/:string", async(req, res)=>{
     
-    const string = parse(req.params.string)
+    const searchString = `%${req.params.string}%`;
     try {
-        const products = await pool.query("SELECT * FROM items WHERE name ILIKE '%$1%' LIMIT 2",[string])
+        const products = await pool.query("SELECT * FROM items WHERE name ILIKE $1 LIMIT 2",[searchString])
         console.log(products.rows)
         res.json(products.rows)
         
