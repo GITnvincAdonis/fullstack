9131b66b892153f6ada997c994e856ed0eb8e1a0
GITnvincAdonis <rav30nad0nis@gmail.com>
2024-11-03 20:03:55 -0400

    search item image feedback, misc styling

--- server/CloudinarySetup.js
+++ server/CloudinarySetup.js
@@ -10,13 +10,29 @@ cloudinary.config({
 
 (async function run(){
     
-const images = [
-    './images/BundleSkinCareRender.png',
-    './images/SinglesSkincareRender.png']
-    
+    // const images = [     './images/BundleSkinCareRender.png',
+    //  './images/SinglesSkincareRender.png']
+
+
+    // const images = ['./images/hyaluronic acid.png','./images/hydrating cleanser.png','./images/Moisturizer.png','./images/Nia.png','./images/squalene.png','./images/untitled.png' ]
+    // for(const image of images){
+    //     const result = await cloudinary.uploader.upload(image)
+    //    console.log(result.public_id);
+    // }
+
+    const images = ['./images/INITA.png' ]
     for(const image of images){
         const result = await cloudinary.uploader.upload(image)
-        console.log(result.secure_url);
+       console.log(result.public_id);
     }
-
+// try {
+//     const vids = ['./images/0001-0522.mp4' ]
+//     for(const vid of vids){
+//         const result = await cloudinary.uploader.upload(vid,{resource_type: 'video'})
+//         console.log(result.public_id);
+//     }
+// } catch (error) {
+//     console.log(error.message)
+// }
+  
 })();
